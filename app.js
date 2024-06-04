61ec281d1efd7f65944cd8213cc48e3aa65ba674
Mangesh98 <mangeshraje55555@gmail.com>
2024-06-04 14:23:03 +0530

    User login, logout functionality added

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
 	res.render("index");
 });
+app.get("/logout", (req, res) => {
+	res.cookie("token", "");
+	res.redirect("/login");
+});
+app.get("/login", (req, res) => {
+	res.render("login");
+});
 app.post("/register", async (req, res) => {
 	let { name, username, email, password, age } = req.body;
 	let user = await userModel.findOne({ email });
@@ -37,6 +44,21 @@ app.post("/register", async (req, res) => {
 			res.cookie("token", token, { httpOnly: true });
 			res.send("Registered Successfully");
 		});
+	});
+});
+app.post("/login", async (req, res) => {
+	let { email, password } = req.body;
+	let user = await userModel.findOne({ email });
+	if (!user) return res.status(500).send("Something went wrong !");
+	bcrypt.compare(password, user.password, (err, result) => {
+		if (err) return res.status(500).send("Something went wrong !");
+		if (!result) return res.status(500).send("Something went wrong !");
+		let token = jwt.sign(
+			{ email: email, userId: user._id },
+			process.env.JWT_SECRET
+		);
+		res.cookie("token", token, { httpOnly: true });
+		res.send("Login Successfully");
 	});
 });
 
